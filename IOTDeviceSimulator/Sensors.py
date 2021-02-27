9fcbdad8d4fe03326adc0d94ffdbbec9ff32104b
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2021-02-27 10:40:43 -0800

    added latch and gps sensor definitions

--- IOTDeviceSimulator/Sensors.py
+++ IOTDeviceSimulator/Sensors.py
@@ -7,6 +7,12 @@ class TemperatureScale(Enum):
     FAHRENHEIT = 2
 
 
+class SensorState(Enum):
+    ON = 1
+    OFF = 2
+    ERROR = 3
+
+
 class Sensor:
     def __init__(self, measured_val=None):
         self.timestamp = dt.utcnow()
@@ -27,7 +33,7 @@ class Sensor:
         return self.__measured_value
 
     @measured_value.setter
-    def measured_value(self, value=0):
+    def measured_value(self, value=None):
         self.__measured_value = value
 
 
@@ -35,3 +41,35 @@ class Temperature(Sensor):
     def __init__(self, measured_value=0.0, temperature_scale=TemperatureScale.CELSIUS):
         Sensor.__init__(self, measured_value)
         self.temperature_scale = temperature_scale
+
+
+class Latch(Sensor):
+    def __init__(self, sensor_state=SensorState.OFF):
+        Sensor.__init__(self, sensor_state)
+
+
+class GPS(Sensor):
+    def __init__(self, lat=37.874772, longi=-122.258674):
+        Sensor.__init__(self)
+        self.latitude = lat
+        self.longitude = longi
+
+    @property
+    def latitude(self):
+        return self.__latitude
+
+    @latitude.setter
+    def latitude(self, value=None):
+        if not isinstance(value, float):
+            raise Exception("Latitude must be a float")
+        self.__latitude = value
+
+    @property
+    def longitude(self):
+        return self.__longitude
+
+    @longitude.setter
+    def longitude(self, value=None):
+        if not isinstance(value, float):
+            raise Exception("Longitude must be a float")
+        self.__longitude = value
